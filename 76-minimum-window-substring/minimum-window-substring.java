39dde655af42bf4ffc35fdffc8e60234e17ccead
Bhavesh Shinde <150414943+7S-Bhavesh@users.noreply.github.com>
2026-07-31 16:56:28 +0530

    Time: 15 ms (58.56%) | Memory: 47.3 MB (39.78%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,32 +1,30 @@
 class Solution {
     public String minWindow(String s, String t) {
-        int l=0;
-        // HashMap<Character,Integer> map=new HashMap<>();
-        int c=0;
-        int n=s.length();
-        int m=t.length();
-        int min=Integer.MAX_VALUE;
-        int sidx=-1;
-        int cnt=0;
-        HashMap<Character,Integer> map2=new HashMap<>();
-        for(int i=0;i<t.length();i++){
-            map2.put(t.charAt(i),map2.getOrDefault(t.charAt(i),0)+1);
-        }
-        for(int r=0;r<n;r++){
-         if(map2.getOrDefault(s.charAt(r),0)>0) cnt++;
-            map2.put(s.charAt(r), map2.getOrDefault(s.charAt(r), 0) - 1);
-           while(cnt==m){
-            if(r-l+1<min){
-            min=r-l+1;
-            sidx=l;
+       HashMap<Character,Integer> map=new HashMap<>();
+       for(int i=0;i<t.length();i++){
+        map.put(t.charAt(i),map.getOrDefault(t.charAt(i),0)+1);
+       }
+       int min=Integer.MAX_VALUE;
+       int c=0;
+       int m=t.length();
+       int n=s.length();
+       int sidx=-1;
+       int l=0;
+       for(int i=0;i<n;i++){
+        if(map.getOrDefault(s.charAt(i),0)>0) c++;
+        map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)-1);
+        while(c==m){
+            if(i-l+1<min){
+                min=i-l+1;
+                sidx=l;
+               
             }
-          map2.put(s.charAt(l),map2.getOrDefault(s.charAt(l),0)+1);
-          if(map2.get(s.charAt(l))>0) cnt--;
+            map.put(s.charAt(l),map.getOrDefault(s.charAt(l),0)+1);
+            if(map.getOrDefault(s.charAt(l),0)>0) c--;
             l++;
-           }
-           
         }
-       
-        return sidx==-1?"":s.substring(sidx,min+sidx);
+       }
+       return sidx==-1?"":s.substring(sidx,sidx+min);
+        
     }
 }
