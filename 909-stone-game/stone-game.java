a7b57fcf263f9bc4c5128ffbad65aa2b40bcf961
Bhavesh Shinde <150414943+7S-Bhavesh@users.noreply.github.com>
2026-08-02 10:12:45 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (48.30%) - LeetSync

---
+++ 909-stone-game/stone-game.java
@@ -0,0 +1,18 @@
+class Solution {
+    // public boolean f(int[] piles,int ind,int turn){
+    //     int n=piles.length;
+    //     if(ind>=n){
+    //         return false;
+    //     }
+    //     if(turn){
+    //         boolean t1=arr[ind]+f(piles,ind+1,false);
+    //         boolean t2=arr[n-ind-1]+f(piles,ind+1,false);
+    //     }
+    //     else{
+
+    //     }
+    // }
+    public boolean stoneGame(int[] piles) {
+        return true;
+    }
+}
