535729f66bfc94690b32f51bc9d360cc29e208e3
Bhavesh Shinde <150414943+7S-Bhavesh@users.noreply.github.com>
2026-08-02 10:36:10 +0530

    Time: 10 ms (10.85%) | Memory: 46.9 MB (10.85%) - LeetSync

--- 909-stone-game/stone-game.java
+++ 909-stone-game/stone-game.java
@@ -1,18 +1,33 @@
 class Solution {
-    // public boolean f(int[] piles,int ind,int turn){
-    //     int n=piles.length;
-    //     if(ind>=n){
-    //         return false;
-    //     }
-    //     if(turn){
-    //         boolean t1=arr[ind]+f(piles,ind+1,false);
-    //         boolean t2=arr[n-ind-1]+f(piles,ind+1,false);
-    //     }
-    //     else{
+    public int f(int[] arr,int i,int j,boolean turn,int sco,int maxi,int[][] dp){
+        int n=arr.length;
+        if(i>j){
+            return sco;
+        }
+        if(dp[i][j]!=0) return dp[i][j];
+        if(turn){
+            int t1=f(arr,i+1,j,false,arr[i]+sco,maxi,dp);
+            int t2=f(arr,i,j-1,false,arr[j]+sco,maxi,dp);
+            dp[i][j]=Math.max(t1,t2);
+        }
+        else{
+            int t3=f(arr,i+1,j,true,sco,maxi,dp);
+            int t4=f(arr,i,j-1,true,sco,maxi,dp);
+            dp[i][j]=Math.max(t3,t4);
+        }
+    return dp[i][j];
 
-    //     }
-    // }
+    }
     public boolean stoneGame(int[] piles) {
-        return true;
+        int maxi=Integer.MIN_VALUE;
+        int n=piles.length;
+        int[][] dp=new int[n+1][n+1];
+        int sco=f(piles,0,n-1,true,0,maxi,dp);
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=piles[i];
+        }
+        int b=sum-sco;
+        return sco>b;
     }
 }
